d0c8c94be201a2662fad6f63ebf5dae8aabbc448
Borja <batutxa@fpsanturtzilh.eus>
2025-01-08 20:34:46 +0100

    first commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,3 +1,4 @@
+import { NiremoduluakModule } from './../konponenteak/niremoduluak.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
@@ -12,7 +13,8 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    NiremoduluakModule
   ],
   declarations: [HomePage]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 
+export interface IPertsona {
+  id:string;
+  izena:string;
+  abizena:string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: false,
 })
+
+
 export class HomePage {
 
+  pertsonak:IPertsona[]=[
+    { id: "ba",
+      izena:"borja",
+      abizena:"atutxa"
+    },
+    {
+      id:"jb",
+      izena:"jon",
+      abizena:"barrutia"
+    }
+  ]
   constructor() {}
 
 }
